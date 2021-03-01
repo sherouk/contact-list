09d736246200db72949c49636238290bd4241c38
Sherouk O <73035812+sherouk@users.noreply.github.com>
2021-03-01 15:56:43 -0600

    project complete

--- src/App.js
+++ src/App.js
@@ -1,41 +1,48 @@
 import './styles.css'
-import React, { useState } from 'react'
-// import ContactCard from './ContactCard'
+import React, { useEffect, useState } from 'react'
+import ContactCard from './ContactCard'
 
 const App = () => {
-  const contacts = [
-    { name: 'Mr. Bubble', email: 'mrbubbles@email.com', age: 43 },
-    { name: 'Mrs. Suds', email: 'mrssuds@email.com', age: 47 },
-    { name: 'Little Soapy', email: 'lilsoapy@email.com', age: 13 }
-  ];
+  const [contacts, setContacts] = useState([])
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=3")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setContacts(data.results)
+      })
+  },[])
+  
+
   return (
     <>
       {contacts.map(contact => (
         <ContactCard
-          avatar='https://via.placeholder.com/150'
-          name={contact.name}
+          avatar={contact.picture.large}
+          name={contact.name.first + "" + contact.name.last}
           email={contact.email}
-          age={contact.age}
+          age={contact.dob.age}
         />
       ))}
     </>
   )
 }
-const ContactCard = props => {
+/* const ContactCard = props => {
   const [showAge, setShowAge] = useState(false)
   return (
-    <div className = "contact-card">
-      <img src={props.avatar} alt = "profile" />
-      <div className = "user-details">
+    <div className ='contact-card'>
+      <img src={props.avatar} alt ='profile' />
+      <div className ='user-details'>
         <p>Name: {props.name}</p>
         <p>Email: {props.email}</p>
-        <button onClick = {() => setShowAge(!showAge)}>
+        <button onClick ={() => setShowAge(!showAge)}>
           Toggle Age
         </button>
         {showAge && <p>Age: {props.age}</p>}
       </div>
     </div>
   )
-}
+} */
 
 export default App
--- src/ContactCard.js
+++ src/ContactCard.js
@@ -1,30 +1,18 @@
 import { useState } from 'react'
-
 const ContactCard = props => {
-  // console.log(props.avatar)
-  // console.log(props.name)
-  // console.log(props.email)
-  // console.log(props.age)
-
-  const contacts = [
-    { name: 'Mr. Bubble', email: 'mrbubbles@email.com', age: 43 },
-    { name: 'Mrs. Suds', email: 'mrssuds@email.com', age: 47 },
-    { name: 'Little Soapy', email: 'lilsoapy@email.com', age: 13 }
-  ]
-
   const [showAge, setShowAge] = useState(false)
-
   return (
-    <>
-      {contacts.map(contact => (
-        <ContactCard
-          avatar='https://via.placeholder.com/150'
-          name={contact.name}
-          email={contact.email}
-          age={contact.age}
-        />
-      ))}
-    </>
+    <div className='contact-card'>
+      <img src={props.avatar} alt='profile' />
+      <div className='user-details'>
+        <p>Name: {props.name}</p>
+        <p>Email: {props.email}</p>
+        <button onClick={() => setShowAge(!showAge)}>
+          Toggle Age
+        </button>
+        {showAge && <p>Age: {props.age}</p>}
+      </div>
+    </div>
   )
 }
 
